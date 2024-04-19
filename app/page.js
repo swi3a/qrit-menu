9ba75634a1ac676bfdf919ac7747059d4e30b72d
swi3a <saadcham97@gmail.com>
2024-04-19 20:49:46 +0100

    changes

--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
 import Qrit from "./components/Qrit";
-import { useRouter } from 'next/router';
 
 export default function Page() {
   return (
--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,3 @@
 const nextConfig = {};
 
 export default nextConfig;
-
-module.exports = {
-    // Enable static HTML export
-    exportPathMap: function() {
-        return {
-            '/': { page: '/' },
-            // Add more pages here
-        };
-    },
-};
