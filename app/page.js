2d55e6f84fb1dae8ee10c1fd52f7a977fdec17d3
swi3a <saadcham97@gmail.com>
2024-04-19 20:08:41 +0100

    chenges

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Qrit from "./components/Qrit";
+import { useRouter } from 'next/router';
 
-export default function Home() {
+export default function Page() {
   return (
     <div>
       <Qrit />
--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,13 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+module.exports = {
+    // Enable static HTML export
+    exportPathMap: function() {
+        return {
+            '/': { page: '/' },
+            // Add more pages here
+        };
+    },
+};
